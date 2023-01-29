a495ffe3867dbdd61950c3672634142397c9c461
Chidimma Onuora <onuorachidimma4@gmail.com>
2023-01-29 21:34:30 +0100

    variable task

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+4
+####################################
+#Write your code below this line 👇
+x = a
+a = b
+b = x
+####################################
+# Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
